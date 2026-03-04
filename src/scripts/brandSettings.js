aacd06653727f12ae338a7c1649f1ce869d1b70c
jazmincorrujedo <corrujedojazmin@gmail.com>
2026-03-04 20:24:33 +0000

    started project

--- src/scripts/brandSettings.js
+++ src/scripts/brandSettings.js
@@ -1,39 +1,39 @@
 export const brandSettings = {
     logo: {
-        src: "", // Add logo image URL here (e.g., "/assets/logo.png")
-        alt: "[Brand Name]",
+        src: "img/redbullenergydrink.svg", // Add logo image URL here (e.g., "/assets/logo.png")
+        alt: "[RedBull]",
         variations: [ // you might have wordmarks / different color variants than what is listed here, that's fine just adjust it
             { 
                 name: "Primary - Light Background", 
-                src: "", // Add image path
+                src: "img/redbull.svg", // Add image path
                 bg: "#ffffff", 
                 darkText: true,
                 description: "Use this version on white or light backgrounds."
             },
             { 
                 name: "Primary - Dark Background", 
-                src: "", // Add image path
+                src: "img/redbull.svg", // Add image path
                 bg: "#333333", 
                 darkText: false,
                 description: "Use this version on dark or brand-colored backgrounds."
             },
             { 
                 name: "Monochrome - Black", 
-                src: "", // Add image path
+                src: "img/redbullblackandwhite2.svg", // Add image path
                 bg: "#ffffff", 
                 darkText: true,
                 description: "For black and white print or single-color applications."
             },
             { 
                 name: "Monochrome - White", 
-                src: "", // Add image path
+                src: "img/redbullblackandwhite.svg", // Add image path
                 bg: "#000000", 
                 darkText: false,
                 description: "For dark backgrounds where color is not permitted."
             }
         ]
     },
-    favicon: "/favicon.svg", // Add favicon URL here
+    favicon: "img/favicon.png", // Add favicon URL here
     typography: {
         primaryFont: "Helvetica Neue",  // this will automatically get set as the page font
         secondaryFont: "Verdana", // dont be a bum, go find some google/adobe fonts you like. just make sure to load them in the stylesheet or Layout astro file 
